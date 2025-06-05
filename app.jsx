f4aa3791463e013fdd9be2172be07fe7722480ac
Alessandra Macedo <alessandramacedo.dev@gmail.com>
2025-06-05 18:59:30 -0300

    app.jsx

---
+++ app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Barchart from "/Barchart";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: "center" }}>Análise Odontológica</h1>
+      <Barchart />
+    </div>
+  );
+}
+
+export default App;
